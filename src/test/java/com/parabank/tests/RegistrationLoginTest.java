c8e9856bf4f96d0ed436701ff1cb4c1a6a18dcb5
Nithy <Nithy@LAPTOP-H7S1G7K9>
2026-01-19 19:16:48 +0530

    Use WebDriverManager and headless Chrome for Jenkins

--- src/test/java/com/parabank/tests/RegistrationLoginTest.java
+++ src/test/java/com/parabank/tests/RegistrationLoginTest.java
@@ -3,6 +3,8 @@ package com.parabank.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -19,14 +21,17 @@ public class RegistrationLoginTest  {
 
     @BeforeMethod
     public void setUp() {
+
+        WebDriverManager.chromedriver().setup();
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
 
         driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 
